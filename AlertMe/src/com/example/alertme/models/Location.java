de034764bbdc3742e8aad7d2347ba61a1474ec0f
prakashkharche <prakashkharche@gmail.com>
2014-10-29 10:52:18 +0530

    :)

--- AlertMe/src/com/example/alertme/models/Location.java
+++ AlertMe/src/com/example/alertme/models/Location.java
@@ -3,23 +3,8 @@ package com.example.alertme.models;
 public class Location 
 {
 	private float lattitude;
-	private String northSouth;
 	private float longitude;
-	private String eastWest;
 	
-	
-	public String getNorthSouth() {
-		return northSouth;
-	}
-	public void setNorthSouth(String northSouth) {
-		this.northSouth = northSouth;
-	}
-	public String getEastWest() {
-		return eastWest;
-	}
-	public void setEastWest(String eastWest) {
-		this.eastWest = eastWest;
-	}
 	public float getLattitude() {
 		return lattitude;
 	}
